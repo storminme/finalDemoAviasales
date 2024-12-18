944427b85e2c839272962935bcad0de7132d0786
storminme <akimovvlad@icloud.com>
2024-12-18 10:01:16 +0300

    1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello, React 17 with TypeScript and IE11 support!</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['core-js/stable', 'regenerator-runtime/runtime', './src/index.tsx'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    },
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: { ie: '11' }, useBuiltIns: 'entry', corejs: 3 }],
+                            '@babel/preset-react',
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        port: 3000,
+        host: '0.0.0.0',
+        allowedHosts: 'all',
+    },
+    target: ['web', 'es5'],
+};
